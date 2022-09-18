52f542f0384836f035b2e40d978174069e46bf3d
Ireedui0223 <moogeeireedui@gmail.com>
2022-09-18 20:53:45 +0800

    me query final

--- src/graphql/queries/user/me.js
+++ src/graphql/queries/user/me.js
@@ -1,10 +1,6 @@
 const { User } = require('../../../model/model');
 
 module.exports = async function (parent, args, context, info) {
-	const user = await User.find(args);
-
-	// console.log("args : ", args);
-	// console.log("getUser");
-	// console.log(user);
-	return user[0];
+	const user = await User.findById(context.user._id);
+	return user;
 };
